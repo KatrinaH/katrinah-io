a63068f01e2147b8a36a3c0fd93597ce7cd8a8a7
KatrinaH <katrina.hussain@gmail.com>
2021-07-24 23:07:35 -0400

    Initial setup + header footer and hero sections

---
+++ src/components/mainLayout.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import '../fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { StaticImage } from 'gatsby-plugin-image'
+import '../components/layout.scss'
+
+
+import { Link } from 'gatsby'
+
+const MainLayout = ({ pageTitle, children  }) => {
+  return (
+    <main>
+      <title>katrinah.io | {pageTitle}</title>
+      <header>
+        <Link to="/">
+          <StaticImage
+            className="logo"
+            alt="Katrina H."
+            src="../images/logo.png"
+          />
+        </Link>
+        <nav>
+          <ul>
+            <li><Link to="/">HOME</Link></li>
+            <li><Link to="/#work">WORK</Link></li>
+            <li><Link to="/#experience">EXPERIENCE</Link></li>
+            <li><Link to="/#connect">CONNECT</Link></li>
+          </ul>
+        </nav>
+      </header>
+      {children}
+      
+      <footer>
+        <p>&copy; {new Date().getFullYear()} &bull; Made with <FontAwesomeIcon icon={['fal', 'heart']} size="1x" /> <a href="https://kamarycreative.com">Kamary Creative</a></p>
+      </footer>
+    </main>
+  )
+}
+
+export default MainLayout
---
+++ src/fontawesome.js
@@ -0,0 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faBehance, faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faHeart } from '@fortawesome/pro-light-svg-icons'
+
+library.add(
+  faBehance,
+  faGithub,
+  faLinkedin,
+  faInstagram,
+  faHeart
+);
