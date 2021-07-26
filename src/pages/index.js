b206ad6f37846e479e38216c2eb905998e0dbf30
KatrinaH <katrina.hussain@gmail.com>
2021-07-25 22:51:29 -0400

    added recent work experience. css changes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,20 +29,36 @@ const IndexPage = () => {
 
       <div id="experience">
         <section>
-          <span className="sectionTitle">Experience</span>
+          <span className="sectionTitle">Recent Experience</span>
+          <div className="job">
+            <h2>2021 &bull; Sr. UI/UX Developer</h2>
+            <h3>Peraton</h3>
+            <p>Worked with a team to design and build a new UI interface for a legacy application.</p>
+          </div>
+          <div className="job">
+            <h2>2018-2021 &bull; Sr. UI/UX Developer</h2>
+            <h3>Northrop Grumman</h3>
+            <p>Contributed to the redesign and UI architecture of a high priority customer facing application. Developed client side UI functionality using React, Redux and Sass. </p>
+          </div>
+          <div className="job">
+            <h2>2015-<span className="textHighlight">Present</span> &bull; Founder + Creative Director</h2>
+            <h3>Kamary Creative, LLC.</h3>
+            <p>Designing and building websites for non-profit agencies, creatives and mission driven entrepreneurs.</p>
+          </div>          
         </section>
       </div>
 
       <div id="connect">
         <section>
-          <span className="sectionTitle">Connect</span>
+          {/* <span className="sectionTitle">Connect</span> */}
+          <p>If you’re interested in working together, <a href="mailto:katrina@kamarycreative.com?subject=Found your website - katrinah.io&hellip;">get in touch</a>!</p>
+
           <ul className="iconList">
             <li><a href="https://www.linkedin.com/in/katrinahussain"><FontAwesomeIcon icon={['fab', 'linkedin-in']} size="lg" className="circle-icon"/></a></li>
             <li><a href="https://github.com/KatrinaH"><FontAwesomeIcon icon={['fab', 'github']} size="lg" className="circle-icon"/></a></li>
             <li><a href="https://www.behance.net/katrinahussain"><FontAwesomeIcon icon={['fab', 'behance']} size="lg" className="circle-icon"/></a></li>
             <li><a href="https://www.instagram.com/kamarycreative"><FontAwesomeIcon icon={['fab', 'instagram']} size="lg" className="circle-icon"/></a></li>
           </ul>
-          <p>If you’re interested in working together, <a href="mailto:katrina@kamarycreative.com">get in touch</a>!</p>
         </section>
       </div>
     </MainLayout>
