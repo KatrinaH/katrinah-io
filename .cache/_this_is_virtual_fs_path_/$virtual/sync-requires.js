1e9a573cc13d405463fe49edba039c30a0b48c84
KatrinaH <katrina.hussain@gmail.com>
2023-06-23 17:03:35 -0400

    Hello to Astro

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/katrinah/Documents/Kamary/Kamary Clients/katrinah-io/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/katrinah/Documents/Kamary/Kamary Clients/katrinah-io/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/katrinah/Documents/Kamary/Kamary Clients/katrinah-io/src/pages/index.js"))
+}
+
